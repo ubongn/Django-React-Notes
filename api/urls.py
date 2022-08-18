9bff7ec4ea835d5a0fcef67bc530eb1be33e2764
Ubong Ntekim <ubongnt@gmail.com>
2022-08-18 14:05:43 +0100

    Adding Django REST Framework

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes, name='routes'),
+    path('notes/', views.getNotes, name='notes'),
+
+    path('notes/<str:pk>/', views.getNote, name="note"),
 ]
